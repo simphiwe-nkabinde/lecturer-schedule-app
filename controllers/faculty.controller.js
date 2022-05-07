f1e4bb42fffc46e47f4a1be225526ab881e3d73d
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-05-07 22:49:16 +0200

    e2e faculty route, controller, view

--- controllers/faculty.controller.js
+++ controllers/faculty.controller.js
@@ -1,8 +1,21 @@
 const dotenv = require('dotenv');
+const { database } = require('pg/lib/defaults');
 dotenv.config()
 const pool = require('../db_connect')
 
 module.exports.faculty_get = (req, res) => {
     //sql query - get faculties: [{faculty_id, name}]
-    res.render('faculty', { title: 'faculty page' });
+    const query = {
+        text: 'SELECT * FROM faculties'
+    }
+
+    pool.query(query.text)
+    .then(data => {
+        if(data.rowCount) {
+            res.render('faculties', { faculties: data.rows})
+        } else { return res.status(404).json('faculties not found')}
+    })
+    .catch(err => {
+        console.log(err);
+    })
 }
