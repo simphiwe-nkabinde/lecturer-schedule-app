534c5874a013beef527107af97907a7384f8cce9
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-05-07 16:40:58 +0200

    change faculty route/controller

--- routes/faculty.route.js
+++ routes/faculty.route.js
@@ -6,7 +6,4 @@ const facultyController = require('../controllers/faculty.controller');
 //faculty page
 router.get('/', facultyController.faculty_get)
 
-//post selected faculty. renders lecturer page
-router.post('/:id', facultyController.faculty_post)
-
 module.exports = router;
