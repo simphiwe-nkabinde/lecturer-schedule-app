3d07ce83b0fb39a02f145e6bdeaa77bc0396e93c
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-05-10 15:25:48 +0200

    create authentication middleware for students and lecturers with cookies

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -12,10 +12,26 @@ module.exports.student = (req, res, next) => {
     if (token) {
         let decoded = jwt.verify(token, 'w')
         if (decoded) {
-            if (decoded.faculty_id) {
+            if (decoded.userType === 'student') {
                 next()
             }
-            
+        } else {
+            return res.status(401).json({error: 'token invalid. Please Login'})
+        }
+    }
+    else {
+        res.status(401).json({error: 'token not found. Please Login'})
+    }
+}
+module.exports.lecturer = (req, res, next) => {
+    let token = req.cookies.jwt
+    console.log(token);
+    if (token) {
+        let decoded = jwt.verify(token, 'w')
+        if (decoded) {
+            if (decoded.userType === 'lecturer') {
+                next()
+            }
         } else {
             return res.status(401).json({error: 'token invalid. Please Login'})
         }
