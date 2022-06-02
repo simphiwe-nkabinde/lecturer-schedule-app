5b0af7ba7177b9e6a43a5bc4404624e020f95f1d
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-06-02 05:38:14 +0200

    fix bug. cors error reading resource, possibly due to cookies

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +18,11 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(cors({
+  origin : "http://lecturer-schedule.herokuapp.com",
+  credentials: true,
+}))
 
 app.use(cookieParser());
 app.use(logger('dev'));
