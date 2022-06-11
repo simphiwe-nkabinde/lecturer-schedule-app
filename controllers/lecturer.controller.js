122fc88e9d6038ff3e2de5599fcfe225ff5de449
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-06-11 11:18:46 +0200

    show faculty name for each lecturer on lecturer admin page

--- controllers/lecturer.controller.js
+++ controllers/lecturer.controller.js
@@ -26,7 +26,7 @@ module.exports.lecturer_get_admin = (req, res) => {
     //sql query - get lecturers: [{lecturer_id, name}]
     const query = {
         // text: 'SELECT lecturers.lecturer_id as id, lecturers.name FROM lecturers INNER JOIN lecturer_department ON lecturer_department.lecturer_id=lecturers.lecturer_id WHERE lecturer_department.department_id = $1',
-        text: 'SELECT lecturer_id as id, name FROM lecturers;',
+        text: 'SELECT lecturers.lecturer_id as id, lecturers.name, faculties.name as faculty FROM lecturers JOIN faculties ON lecturers.faculty_id = faculties.faculty_id;',
         // values: [departmentId]
     }
 
